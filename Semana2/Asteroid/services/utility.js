6b3ebbb15d449460953317f0a7b8e138dfbc987c
pathrl <patricia.rl10@hotmail.com>
2017-11-13 19:04:21 +0100

    utilities

--- Semana2/Asteroid/services/utility.js
+++ Semana2/Asteroid/services/utility.js
@@ -4,11 +4,3 @@ function loadImage(ctx, image, posX, posY, width, heigth, src) {
   }
   image.src = src;
 }
-
-/* ROTAR IMAGEN
-  context.translate(x, y);
-  context.rotate(angleInRadians);
-  context.drawImage(image, -width / 2, -height / 2, width, height);
-  context.rotate(-angleInRadians);
-  context.translate(-x, -y);
-*/
