9c46f29845ca5642ba5996b5bb205c172d2524db
pathrl <patricia.rl10@hotmail.com>
2017-11-27 19:32:33 +0100

    First example worker

--- Semana5/dia16/workers/currante.js
+++ Semana5/dia16/workers/currante.js
@@ -1,4 +1,6 @@
-self.onmessage(function(){
-  console.log('soy el currante y he recibido un mensaje');
-  self.postMessage('te devuelvo un mensaje desde el currante');
-});
+self.onmessage = function(msg){
+  //recibo el mensaje
+  console.log('soy el currante y he recibido el mensaje: ' + msg.data);
+  //contesto enviando un mensaje
+  self.postMessage('Adios');
+};
