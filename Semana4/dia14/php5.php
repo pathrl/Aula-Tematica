c23be5a770792b51e4bc16bb83f9bbdd68a630ca
pathrl <patricia.rl10@hotmail.com>
2017-11-27 19:13:49 +0100

    day 14

--- Semana4/dia14/php5.php
+++ Semana4/dia14/php5.php
@@ -1,11 +1,12 @@
 <?php
     $imgfondo = imagecreatefrompng("recursos/fondo.png");
-    $imgcarretera = imagecreatefrompng("recursos/road.png");
-
+    $imgcarretera = imagecreatefrompng("recursos/road2.png");
     header('Content-Type: image/png');
-    
-    imagepng($imgfondo);
-
+    for($i = 0;$i<100;$i++){
+        $im2 = imagecopy($imgfondo, $imgcarretera, rand(0,512),rand(0,512), 0, 0, 64, 64);
+    }
+    imagepng($imgfondo,"fondogenerado.png");
     imagedestroy($imgfondo);
     imagedestroy($imgcarretera);
 ?>
+ok
