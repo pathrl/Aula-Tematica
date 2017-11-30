f6ba29538a0fc18da41c291c6f8d5c12c4d07490
pathrl <patricia.rl10@hotmail.com>
2017-11-30 19:08:56 +0100

    example 6 day 17

--- Semana5/dia17/example3/currante6.js
+++ Semana5/dia17/example3/currante6.js
@@ -1,29 +1,14 @@
 var temporizador;
 var numero = 2;
 
-self.onmessage = function(e){
-  console.log("soy el currante y he recibido un mensaje: "+e.data)
+self.onmessage = function(e) {
+  var datos = e.data;
+  var partido = datos.split(',');
+  var serializado;
+  for(var i = 0; i < partido.length; i++) {
+    partido[i] = 255 - partido[i];
+    serializado += partido[i] + ',';
+  }
+  postMessage(serializado);
   //postMessage("Adios")
-  temporizador = setTimeout("bucle()",30)
-}
-function bucle(){
-  numero *= 2;
-  postMessage(numero)
-  clearTimeout(temporizador)
-  temporizador = setTimeout("bucle()",30)
-}
-
-var temporizador;
-var numero = 2;
-
-self.onmessage = function(e){
-    console.log("soy el currante y he recibido un mensaje: "+e.data)
-    //postMessage("Adios")
-    temporizador = setTimeout("bucle()",30)
-}
-function bucle(){
-    numero *= 2;
-    postMessage(numero)
-    clearTimeout(temporizador)
-    temporizador = setTimeout("bucle()",30)
 }
